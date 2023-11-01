186201cf38d0e7475890f379a23f82e3ca5b9e8f
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-11-01 20:48:19 +0300

    Fix linters errors

--- morse_code.rb
+++ morse_code.rb
@@ -36,9 +36,9 @@ def decode_word(word)
 end
 
 def decode_message(message)
-    @message = message.split('   ')
-    @msg = @message.map { |w| decode_word(w) }
-    @msg.join(' ')
-  end  
+  @message = message.split('  ')
+  @msg = @message.map { |w| decode_word(w) }
+  @msg.join(' ')
+end
 
 puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
